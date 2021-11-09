db9bed31594cccccc407d8866101b7a82529f4b5
10RE <lorenlin@umich.edu>
2021-11-09 17:27:15 -0500

    dev-change weapon display to script

---
+++ scripts/json2str.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+files = os.listdir("../jsons")
+for file in files:
+    f_in = open("../jsons/" + file, "r")
+    try:
+        f_out = open("../jsons_str/" + file[:-5] + ".js", "x")
+    except FileExistsError:
+        f_out = open("../jsons_str/" + file[:-5] + ".js", "w")
+    data = json.load(f_in)
+    data = json.dumps(data)
+    f_out.write(file[:-5] + " = \'" + str(data) + "\';")
+    f_in.close()
+    f_out.close()
+
---
+++ scripts/weapons.js
@@ -0,0 +1,30 @@
+let weapon_container = document.getElementById("weapon_container");
+
+let data = JSON.parse(weapons_data);
+data = data["weapons_data"];
+for (let weapon of data) {
+    let weapon_name = weapon["name"];
+    let item_node = document.createElement("div");
+    item_node.classList.add("list_item");
+    
+    let item_img_node = document.createElement("img");
+    item_img_node.setAttribute("src", "../imgs/CSGO_" + weapon_name + "_Inventory.png");
+    item_img_node.setAttribute("alt", weapon_name);
+    item_img_node.classList.add("item_img");
+
+    let item_name_node = document.createElement("div");
+    item_name_node.classList.add("item_name");
+    item_name_node.textContent = weapon_name;
+
+    let item_description_node = document.createElement("div");
+    item_description_node.classList.add("item_descript", "descript");
+    item_description_node.textContent = weapon["description"];
+
+    item_node.append(item_img_node);
+    item_node.append(item_name_node);
+    item_node.append(item_description_node);
+
+    weapon_container.append(item_node);
+    console.log(weapon);
+}
+
