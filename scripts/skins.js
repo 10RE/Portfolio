8b13708df56129b2909528560ba6ddc95b3e4a44
10RE <lorenlin@umich.edu>
2021-11-15 22:24:33 -0500

    dev-update skin page clickable

--- scripts/skins.js
+++ scripts/skins.js
@@ -1,17 +1,38 @@
+let PAGE_DISPLAY_NUMBER = 10;
+
 let skin_container = document.getElementById("skin_container");
+let skin_page_navigation = document.getElementById("skin_page_navigation");
 let skins_data_local = readWeaponData(skins_data);
+
 console.log(skins_data_local);
-renderSkin();
+renderSkin(1);
+renderPageNav(1);
 
 function readWeaponData(data_in) {
     let data = JSON.parse(data_in);
     return data["skins_data"];
 }
 
-function renderSkin() {
-    clearSkins();
+function renderPageNav (page) {
+    clearChildNodes(skin_page_navigation);
+    for (let idx = 1; idx < skins_data_local.length / PAGE_DISPLAY_NUMBER; idx ++) {
+        let page_nav_node = document.createElement("p");
+        page_nav_node.classList.add("clickable", "page_nav", "hover_link");
+        if (idx === page ) {
+            page_nav_node.classList.add("selected_link");
+        }
+        page_nav_node.textContent = idx;
+        page_nav_node.addEventListener("click", () => {
+            renderSkin(idx);
+            renderPageNav(idx);
+        });
+        skin_page_navigation.append(page_nav_node);
+    }
+}
 
-    for (let skin of skins_data_local) {
+function renderSkin(page) {
+    clearChildNodes(skin_container);
+    for (let skin of skins_data_local.slice((page-1) * PAGE_DISPLAY_NUMBER, page * PAGE_DISPLAY_NUMBER)) {
         let skin_name = skin["name"];
         let item_node = document.createElement("div");
         item_node.classList.add("list_item");
@@ -29,8 +50,12 @@ function renderSkin() {
         item_price_div_node.classList.add("price_div");
 
         let item_price_node = document.createElement("div");
-        item_price_node.classList.add("price");
+        item_price_node.classList.add("price", "clickable");
         item_price_node.textContent = "$" + skin["price"];
+
+        item_price_node.addEventListener("click", () => {
+            window.open(skin["link"], "_blank");
+        });
 
         item_price_div_node.append(item_price_node);
         item_node.append(item_img_node);
@@ -42,12 +67,12 @@ function renderSkin() {
     }
 }
 
-function clearSkins () {
-    if (!skin_container.firstChild) {
+function clearChildNodes (node) {
+    if (!node.firstChild) {
         return;
     }
-    while (skin_container.firstChild) {
-        skin_container.removeChild(skin_container.firstChild);
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);
     }
 }
 
